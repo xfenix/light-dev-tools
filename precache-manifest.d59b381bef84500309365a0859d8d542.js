7361e1df09f3c49e4e3be2813415c30c82b5ebb9
Denis Anikin <ad@xfenix.ru>
2020-08-19 23:25:48 +0300

    Updates

---
+++ precache-manifest.d59b381bef84500309365a0859d8d542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4429bf3827a4516cab43cf6e45b7a84d",
+    "url": "/light-dev-tools/index.html"
+  },
+  {
+    "revision": "b7ef8053b1801bc53bd8",
+    "url": "/light-dev-tools/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "b7ef8053b1801bc53bd8",
+    "url": "/light-dev-tools/static/js/2.e000db02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/light-dev-tools/static/js/2.e000db02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88cbbb5e15bc0e283edf",
+    "url": "/light-dev-tools/static/js/main.aacaa81c.chunk.js"
+  },
+  {
+    "revision": "89e22c4be8a031a676eb",
+    "url": "/light-dev-tools/static/js/runtime-main.28d9f15f.js"
+  }
+]);
